097a7c1222fe9360af394128a764768c124dd0b7
StevenTibbo <steven@tibbo.com>
2023-12-06 18:00:05 +0800

    Fix firmware upgrade issue
    Fix bug on IOT CENTRAL "Reported" and "Desired" Twin Casing Property not synchronized

--- OSS_Azure/Release_Files/Release_Tools/release.bat
+++ OSS_Azure/Release_Files/Release_Tools/release.bat
@@ -1,6 +1,1 @@
-lzo1x-999.exe ..\KLM601.tpc ..\binary\tmp\compAPP.bin
-copy /b ..\tios\tios-KLM106-3_90_05.bin + /b ..\binary\tmp\compAPP.bin ..\binary\tmp\TiOScompAPP.bin
-wa2000_firmware_concat --tios_app_c "..\binary\tmp\TiOScompAPP.bin" --o ..\binary\KLM106_comp\OC53-7_46-TEST.tcc
-copy /b ..\tios\tios-KLM106-3_90_05.bin + /b ..\KLM601.tpc ..\binary\tmp\TiOSAPP.bin
-wa2000_firmware_concat --tios_app "..\binary\tmp\TiOSAPP.bin" --o ..\binary\KLM106_uncomp\OC53-7_45-TIOS-3_90_05_WA_N9MON_uncomp.tcu
-copy /b ..\Platforms\WM2000\firmware\tios-wm2000-4_02_01.bin + /b ..\Platforms\WM2000\firmware\app0\WM2000-Companion.tpc + /b filler1.dat + /b ..\OSS_azure.tpc  ..\binary\padded.bin
+lzo1x-999.exe  ..\..\OSS_Azure_APP1\tmp\TiOSAPP.bin ..\..\Release_Files\Compressed_For_WiFi-4G_Upgrades\OSS_Azure_APP1_2-00-05.bin
